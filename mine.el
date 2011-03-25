bf12ef8a640778f4c6d64f9fc14c73480895d00f
Anastasios Germanidis <agermanidis@gmail.com>
2011-03-25 22:34:03 +0000

    Added rename function

--- mine.el
+++ mine.el
@@ -1,12 +1,8 @@
 ;; loading modules
 
-(require 'inf-haskell)
-(require 'pycomplete)
-(require 'ipython)
 (require 'python-automatize)
-(require 'smooth-scrolling)
 
-;; settings
+ ;; settings
 
 (tool-bar-mode nil)                            ; No toolbar
 (menu-bar-mode nil)                            ; No menu bar
@@ -162,9 +158,20 @@
 			    (local-set-key "\C-cl" 'js-load-file-and-go)
 			    ))
 
-
-
-
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file to NEW-NAME"
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named %s already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
 
 (provide 'mine)
 
